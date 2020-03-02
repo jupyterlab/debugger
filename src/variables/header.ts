41f1cce191113f9f17ba8c5bbf4177bd73467d6d
Borys Palka <borys.palka@codete.com>
2020-03-02 11:56:53 +0100

    add ToolbarIcon to headers

--- src/variables/header.ts
+++ src/variables/header.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { Toolbar } from '@jupyterlab/apputils';
+import { Toolbar, ToolbarButton } from '@jupyterlab/apputils';
 
 import { PanelLayout, Widget } from '@lumino/widgets';
 
@@ -15,10 +15,19 @@ export class VariablesHeader extends Widget {
   constructor() {
     super({ node: document.createElement('header') });
 
+    const button = new ToolbarButton({
+      className: 'jp-SwitchButton',
+      iconClass: 'jp-ToggleSwitch',
+      onClick: function() {
+        console.log('hi :)');
+      },
+      tooltip: 'Table / Tree View'
+    });
     const title = new Widget({ node: document.createElement('h2') });
     title.node.textContent = 'Variables';
 
     const layout = new PanelLayout();
+    layout.addWidget(button);
     layout.addWidget(title);
     layout.addWidget(this.toolbar);
     this.layout = layout;
